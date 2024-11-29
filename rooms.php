c3a7717afb714c68ae9aadc2368412e078a00dc3
T5a6 <202101300@stu.uob.edu.bh>
2024-11-29 22:30:18 +0300

    Create rooms.php

---
+++ rooms.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Building Map</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<header>
+    <h1>header</h1>
+</header>
+<main>
+    <div class= "image_container">
+    <div class="grid_layout_parent">
+    <div class=grid_layout>
+        <div class="section section1" title="Information Systems" href='#'></div>
+        <div class="section section2" title="Computer Science" href='#'></div>
+        <div class="section section3" title="Computer Engineering" href='#'></div>
+    </div> </div>
+    <img id="building_map"  src="images/building-map-fixed.jpg" alt="Building Map">
+
+
+    </div>
+</main>
+<footer>
+    footer
+</footer>
