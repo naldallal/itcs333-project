6db859014a26c55ff6c471fddd8e099064e49d99
M2ryamal7asan <mariamal7asan@gmail.com>
2024-12-02 11:45:28 +0300

    fix

--- userprofileM.php
+++ userprofileM.php
@@ -38,24 +38,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     header("Location: profile.php");
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Profile</title>
-</head>
-<body>
-    <h1>Edit your Profile</h1>
-    <form method="POST" enctype="multipart/form-data">
-        <label>New Username: <input type="text" name="username" value="<?= htmlspecialchars($user['username']) ?>" required></label><br>
-        <label>New Password  <input type="password" name="password"></label><br>
-        <?php if ($user['profile_picture']): ?>
-            <img src="upload/<?= htmlspecialchars($user['profile_picture']) ?>" alt="Profile Picture" width="100"><br>
-        <?php endif; ?>
-        <label> Profile Picture: <input type="file" name="profile_picture"></label><br>
-        <button type="submit">Save Changes</button>
-    </form>
-    <a href="logout.php">Logout</a>
-</body>
-</html>
