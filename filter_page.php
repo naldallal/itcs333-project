498b351b20a199f8dc18a9fdffb6f0b02640728d
T5a6 <202101300@stu.uob.edu.bh>
2024-12-04 20:03:56 +0300

    Update filter_page.php

--- filter_page.php
+++ filter_page.php
@@ -63,7 +63,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css_filter_page.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <title>Filter Rooms</title>
     
